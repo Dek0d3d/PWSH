d8c3081872b15eb42777a15d02b5b6d80c6b3a58
Jon Luke <jon.w.luke@outlook.com>
2022-06-06 07:54:55 -0700

    Update file

--- Work In Progress/AzUserAdminTool.ps1
+++ Work In Progress/AzUserAdminTool.ps1
@@ -2,6 +2,7 @@
 # This Script Renders a GUI which will assist in running commands. #
 ####################################################################
 $Script:tenant = ""
+$Script:connected = $false
 
 Function CheckModule{
     if(!(Get-InstalledModule | Where-Object Name -like "*Microsoft.Graph*")){
@@ -31,14 +32,60 @@ Function ConnectModule {
 
 Function GetConnection {
     $Script:tenant = Get-MgOrganization | Select-Object -Property *
+    $Script:connected = $true
 }
 
 Function DisplayTenantInfo {
+    param([Parameter()][System.Windows.Forms.GroupBox]$gpBox)
+
     $TenantLabel = New-Object System.Windows.Forms.Label
-    $TenantLabel.Text = $Script:tenant
-    $Tenantlabel.Width = 400
-    $TenantLabel.Height = 300
-    $flowPanel.Controls.Add($TenantLabel)
+    $TenantLabel.Text = "$(($Script:tenant).DisplayName) : $(($Script:tenant).Id)"
+    $TenantLabel.Width = 275
+    $TenantLabel.Height = 25
+    
+    $UserLabel = New-Object System.Windows.Forms.Label
+    $UserLabel.Text = "$((Get-MgContext).Account)"
+    $UserLabel.Width = 275
+    $UserLabel.Height = 25
+    
+    $gpBox.Controls.Add($TenantLabel)
+    $gpBox.Controls.Add($UserLabel)
+}
+
+Function DisplayConnection {
+    param([Parameter()][System.Windows.Forms.FlowLayoutPanel]$flowPanel)
+
+    $modLabel = New-Object System.Windows.Forms.Label
+    $modLabel.Text = "Microsoft Graph Powershell SDK installed:" + $(CheckModule)
+    $modlabel.Width = 300
+    $modLabel.Height = 25
+
+    if(CheckModule){
+        $connectButton = New-Object System.Windows.Forms.Button
+        $connectButton.Size = New-Object System.Drawing.Size(120,23)
+        $connectButton.Text = "Connect"
+        if($Script:tenant){
+            $connectButton.Enabled = $false
+        } else {
+            $connectButton.Add_Click({ConnectModule})
+        }
+        $modButton = $connectButton
+    } else {
+        $installButton = New-Object System.Windows.Forms.Button
+        $installButton.Size = New-Object System.Drawing.Size(120,23)
+        $installButton.Text = "Install"
+        $installButton.Add_Click({InstallModule})
+        $modButton = $installButton
+    }
+
+    $gpBox = New-Object System.Windows.Forms.GroupBox
+    $gpBox.Controls.Add($modLabel)
+    $gpBox.Controls.Add($modButton)
+
+    if($Script:connected){
+        DisplayTenantInfo $gpBox
+    }
+
 }
 
 Function GenerateGUI {
@@ -46,46 +93,24 @@ Function GenerateGUI {
     Add-Type -AssemblyName System.Windows.Forms
     [System.Windows.Forms.Application]::EnableVisualStyles()
 
-    $script:gui = New-Object System.Windows.Forms.Form
+    $gui = New-Object System.Windows.Forms.Form
     $gui.Text = 'Azure AD User Admin Tool'
     $gui.Width = 600
     $gui.Height = 400
 
     $flowPanel = New-Object System.Windows.Forms.FlowLayoutPanel
-    $flowPanel.AutoSize = $true
-    
-    $Label = New-Object System.Windows.Forms.Label
-    $Label.Text = "Microsoft Graph Powershell SDK installed:" + $(CheckModule)
-    $label.Width = 400
-    $Label.Height = 25
-    $flowPanel.Controls.Add($Label)
+    $flowPanel.Width = 590
+    $flowPanel.Height = 390
 
-    $connectButton = New-Object System.Windows.Forms.Button
-    $connectButton.Size = New-Object System.Drawing.Size(120,23)
-    $connectButton.Text = "Connect"
-    if($Global:tenant){
-        $connectButton.Enabled = $false
-    } else {
-        $connectButton.Add_Click({ConnectModule})
-    }
-    
-    $installButton = New-Object System.Windows.Forms.Button
-    $installButton.Size = New-Object System.Drawing.Size(120,23)
-    $installButton.Text = "Install"
-    $installButton.Add_Click({InstallModule})
+    DisplayConnection $flowPanel
 
-    $showButton = if(CheckModule){
-        $connectButton
-    }
-    else {
-        $installButton
-    }
-
-    $flowPanel.Controls.Add($Label)
-    $flowPanel.Controls.Add($showButton)
     $gui.Controls.Add($flowPanel)
     $gui.ShowDialog()
 
 }
 
-GenerateGUI
+try {
+    GenerateGUI
+} finally {
+    Write-Host Good Job
+}
